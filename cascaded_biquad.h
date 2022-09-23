c9c0e2e598dbe44e78d423f9e49d540e258b26b2
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-09-23 14:15:32 +0200

    add some comments

---
+++ cascaded_biquad.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace cascaded_biquad {
+
+  template<int stages, typename coefficient_t, typename state_t, typename sample_t> sample_t process(coefficient_t *coefficients, state_t* state, sample_t in) {
+    sample_t intermediate = in;
+    for (int stage = 0; stage < stages; ++stage) {
+      sample_t stage_in = intermediate;
+      sample_t stage_out = stage_in * coefficients[6*stage+0] + state[2*stage+0];
+      state[2*stage+0] = state[2*stage+1] + coefficients[6*stage+1] * stage_in - coefficients[6*stage+4] * stage_out;
+      state[2*stage+1] = coefficients[6*stage+2] * stage_in - coefficients[6*stage+5] * stage_out;
+      intermediate = stage_out;
+    }
+    return intermediate;
+  }
+
+  template<int stages, typename state_t> state_t* state() {
+    return new state_t[2*stages];
+  }
+
+}
---
+++ test.cc
@@ -0,0 +1,36 @@
+#include "cascaded_biquad.h"
+#include <iostream>
+
+// These coefficients were generated with MATLAB:
+// f = designfilt('lowpassiir', 'PassbandFrequency', 0.01, 'StopbandFrequency', 0.02, 'StopbandAttenuation', 32);
+// f.Coefficients
+float coefficients[] = {
+   0.000341790148053,   0.000683580296107,   0.000341790148053,   1.000000000000000,  -1.982247725220660,   0.983614885812873,
+   0.000336817881561,   0.000673635763122,   0.000336817881561,   1.000000000000000,  -1.953410545448979,   0.954757816975223,
+   0.000333460820498,   0.000666921640997,   0.000333460820498,   1.000000000000000,  -1.933940918565828,   0.935274761847821,
+   0.018228517368043,   0.018228517368043,                   0,   1.000000000000000,  -0.963542965263913,                   0
+};
+
+#define NUM_SAMPLES 1000
+
+int main()
+{
+  namespace cb = cascaded_biquad;
+  float *state = cb::state<4, float>();
+
+  float input[NUM_SAMPLES];
+
+  // Our input is a step function
+  for (int index = 0; index < NUM_SAMPLES; ++index) {
+    if (index < NUM_SAMPLES/2) input[index] = 0; else input[index] = 1;
+  } 
+
+  for (int index = 0; index < NUM_SAMPLES; ++index) {
+    std::cout << cb::process<4, float, float, float>(coefficients, state, input[index]) << "\n";
+  }
+
+  delete[] state;
+
+  return 0;
+}
+
