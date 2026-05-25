582fe82fda38a86c6fe21ea9737a77915f3ae06e
Florian Paul Schmidt <mista.tapas@gmx.net>
2026-05-25 13:57:40 +0200

    rework everything

--- cascaded_biquad.h
+++ cascaded_biquad.h
@@ -1,36 +1,68 @@
 #pragma once
 
 #include <cstring>
+#include <array>
 
 namespace cascaded_biquad 
 {
-  // The coefficients are expected to be normalized, i.e. the fifth column
-  // has to be ones. If that is not the case you need to to normalize them
-  // beforehand!
-  // Additionally they are stored as a 1-D array in row-major order. I.e. 
-  // the first 5 elements are the entries in the first row.
-  template<int stages, typename coefficient_t, typename state_t, typename sample_t> 
-  sample_t process(const coefficient_t *coefficients, state_t* state, coefficient_t gain, const sample_t in) 
+  template<typename coefficient_t>
+  struct coefficients
   {
-    sample_t intermediate = in;
-    for (int stage = 0; stage < stages; ++stage) 
-    {
-      const sample_t stage_in = intermediate;
-      const sample_t stage_out = stage_in * coefficients[5*stage+0] + state[2*stage+0];
-      state[2*stage+0] = state[2*stage+1] + coefficients[5*stage+1] * stage_in - coefficients[5*stage+3] * stage_out;
-      state[2*stage+1] = coefficients[5*stage+2] * stage_in - coefficients[5*stage+4] * stage_out;
-      intermediate = stage_out;
-    }
-    return gain * intermediate;
-  }
+    coefficient_t b0;
+    coefficient_t b1;
+    coefficient_t b2;
 
-  // Produce an array suitable as filter state for a filter consisting of 
-  // number_of_stages stages.  Delete the array with delete[] instead of delete.
-  template<int number_of_stages, typename state_t> 
-  state_t* state() 
+    coefficient_t a0;
+    coefficient_t a1;
+    coefficient_t a2;
+  };
+
+  template<typename coefficient_t>
+  struct normalized_coefficients
   {
-    state_t *state = new state_t[2*number_of_stages];
-    memset(state, 0, sizeof(state_t)*2*number_of_stages);
-    return state;
-  }
+    coefficient_t b0;
+    coefficient_t b1;
+    coefficient_t b2;
+
+    coefficient_t a1;
+    coefficient_t a2;
+  };
+
+  template<typename coefficient_t>
+  normalized_coefficients<coefficient_t> normalize_coefficients(coefficients<coefficient_t> const & c)
+  {
+    return { c.a1 / c.a0, c.a2 / c.a0, c.b0 / c.a0, c.b1 / c.a0, c.b2 / c.a0 };
+  };
+  
+  template<int stages, typename coefficient_t = float, typename state_t = coefficient_t, typename sample_t = coefficient_t>
+  struct direct_form1
+  {
+    std::array<normalized_coefficients<coefficient_t>, stages> coefficients;
+    coefficient_t gain;
+    std::array<std::array<state_t, 2>, stages> previous_inputs;
+    std::array<std::array<state_t, 2>, stages> previous_outputs;
+
+    inline sample_t process(float const input)
+    {
+      sample_t intermediate = input;
+      for (int stage = 0; stage < stages; ++stage)
+      {
+        auto const & c = coefficients[stage];
+        auto & pi = previous_inputs[stage];
+        auto & po = previous_outputs[stage];
+
+        const sample_t out = c.b0 * intermediate + c.b1 * pi[0] + c.b2 * pi[1] - c.a1 * po[0] - c.a2 * po[1];
+
+        pi[1] = pi[0];
+        po[1] = po[0];
+        pi[0] = intermediate;
+        po[0] = out; 
+
+        intermediate = out;    
+      }
+
+      return gain * intermediate;
+    }
+  };
+
 }
